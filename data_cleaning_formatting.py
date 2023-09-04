42d2f374a2514fb2babdb7b43396ddec9ba14026
Karolis Petkevicius <karolispetkevicius7@gmail.com>
2023-09-05 01:22:33 +0300

    added data formatting script(version1)

--- data_cleaning_formatting.py
+++ data_cleaning_formatting.py
@@ -1,4 +1,27 @@
 import pandas as pd
-import numpy as np
+import pymongo
+
+pd.set_option('display.max_columns', None)
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client['Slack_Apps']
+collection = db['slack_apps']
+data = collection.find()
+
+
+# Convert the cursor to a list of dictionaries and create a DataFrame
+df = pd.DataFrame(list(data), columns=['date', 'category', 'ranking', 'app_name', 'supported_languages', 'pricing', 'app_description'])
+
+
+df['date'] = pd.to_datetime(df['date'])
+df['ranking'] = pd.to_numeric(df['ranking'], errors='coerce')
+
+
+
+df_sorted = df.sort_values(by=['category', 'ranking'])
+
+
+category_test = df_sorted[df_sorted['category'] == 'Design']
+print(category_test)
 
 
